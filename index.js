3d1f659b4640b435464d780f39729ebc5c7d750a
JoelGW1 <155395766+JoelGW1@users.noreply.github.com>
2024-01-28 00:05:51 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,43 +1,137 @@
-const  tapBD = document.getElementById('tapBD');
-const bd = document.getElementById('bd');
-const  bdWindow = document.getElementById('bdWindow');
-const  window2 = document.getElementById('window2');
+let stars = document.getElementById("stars");
+let s1 = document.getElementsByClassName("s1");
+let s2 = document.getElementsByClassName("s2");
+let s3 = document.getElementsByClassName("s3");
+let s4 = document.getElementsByClassName("s4");
+let lifeSpan = 80;
+let age = document.getElementById("age").value = 0;
 
-window2.style.display = 'none';
 
-let weeksLived;
+function drawSky(years) {
+    {
 
-function reset() {
-    bd.value = 'yyyy-mm-dd';
+        for (let i = 0; i < ((years*0.5)*52); i++) {
+            stars.insertAdjacentHTML("afterbegin","<div class='s1'> </div>");
+        }
+        
+        for (let i = 0; i < s1.length; i++) {
+            drawStar(s1, i, 0);
+        }
+        }
+        
+        {
+        for (let i = 0; i < ((years*0.4)*52); i++) {
+            stars.insertAdjacentHTML("afterbegin","<div class='s2'> </div>");
+        }
+        
+        for (let i = 0; i < s2.length; i++) {
+            drawStar(s2, i, 0);
+        }
+        }
+        
+        {
+        for (let i = 0; i < ((years*0.05)*52); i++) {
+            stars.insertAdjacentHTML("afterbegin","<div class='s3'> </div>");
+        }
+        
+        for (let i = 0; i < s3.length; i++) {
+            drawStar(s3, i, 0);
+        }
+        }
+        
+        {
+        for (let i = 0; i < ((years*0.05)*52); i++) {
+            stars.insertAdjacentHTML("afterbegin","<div class='s4'> </div>");
+        }
+        
+        for (let i = 0; i < s4.length; i++) {
+            drawStar(s4, i, 0);
+        }
+        }
+        setTimeout(start,1);
 }
 
-function hide() {
-    if (bd.value.length != 0) {
-        let dateObj = new Date(bd.value);
-        weeksLived = Math.trunc((new Date() - dateObj.getTime())/604800000);
-        bdWindow.style.display = 'none';
-        viewWeeks();
+function drawStar(type, i, opacityV) {
+    let thisStar = type[i];
+    randomTop = getRandomPosition(0, window.innerHeight -5);
+    randomLeft = getRandomPosition(0, window.innerWidth -5);
+    thisStar.style.top = randomTop+"px";
+    thisStar.style.left = randomLeft+"px";
+    thisStar.style.opacity = opacityV;
+}
+
+function getRandomPosition(min,max) {
+    return Math.random() * (max-min) + min;
+}
+function play() {
+    document.getElementById("two").scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"});
+}
+function start() {
+    let age = document.getElementById("age").value;
+    age = Number(age);
+    if (age <= 80 && age >= 0){
+        for (let i = 0; i < lifeSpan*0.5*52; i++) {
+            s1[i].style.opacity = '0.6';
+        }
+        for (let i = 0; i < lifeSpan*0.4*52; i++) {
+            s2[i].style.opacity = '0.7';
+        }
+        for (let i = 0; i < lifeSpan*0.05*52; i++) {
+            s3[i].style.opacity = '0.8';
+        }
+        for (let i = 0; i < lifeSpan*0.05*52; i++) {
+            s4[i].style.opacity = '1';
+        }
+        
+        for (let i = 0; i < age*0.5*52; i++) {
+            s1[i].style.opacity = '0';
+        }
+        for (let i = 0; i < age*0.4*52; i++) {
+            s2[i].style.opacity = '0';
+        }
+        for (let i = 0; i < age*0.05*52; i++) {
+            s3[i].style.opacity = '0';
+        }
+        for (let i = 0; i < age*0.05*52; i++) {
+            s4[i].style.opacity = '0';
+        }
+        } else {
+            alert("age >= 0 or age <= 80");
     }
 }
 
-function viewWeeks() {
-    window2.style.display = 'flex';
-    const squaresPast = document.getElementById('squaresPast');
-    const squaresLive = document.getElementById('squaresLive');
-
-    for (let i = 0; i < weeksLived; i++) {
-        setTimeout(addPast,.6*i);
-    }
-    
-    for (let i = 0; i < 4160 - weeksLived; i++) {
-        setTimeout(addLive,.6*i);
-    }
+function animate2() {
+    document.getElementById("one").innerHTML = ``;
+    age = Number(document.getElementById("age").value);
+    start();
+    document.getElementById("two").innerHTML = `<h1>These are your remaining weeks</h1>`;
+    setTimeout(function() {
+    for (let i =0; i <= (lifeSpan-age);i++){
+        setTimeout(function() {
+        document.getElementById("two").innerHTML = `<h1 id="counter">${age} years</h1>`;
+        console.log(age);
+        for (let i = 0; i < age*0.5*52; i++) {
+            s1[i].style.transition = "opacity 0.1s ease-in-out";
+            s1[i].style.opacity = '0';
+        }
+        for (let i = 0; i < age*0.4*52; i++) {
+            s2[i].style.transition = "opacity 0.1s ease-in-out";
+            s2[i].style.opacity = '0';
+        }
+        for (let i = 0; i < age*0.05*52; i++) {
+            s3[i].style.transition = "opacity 0.1s ease-in-out";
+            s3[i].style.opacity = '0';
+        }
+        for (let i = 0; i < age*0.05*52; i++) {
+            s4[i].style.transition = "opacity 0.1s ease-in-out";
+            s4[i].style.opacity = '0';
+        }
+        if (age == 80) {setTimeout(function() {location.reload()},1500)}
+        age++;
+        }, i*200)};}, 2500);
 }
 
-function addPast() {
-    squaresPast.insertAdjacentHTML('beforeend',`<div class="darkSquare"></div>`);
-}
-
-function addLive() {
-    squaresLive.insertAdjacentHTML('beforeend',`<div class="lightSquare"></div>`);
+function toTop() {
+    let container = document.getElementById("container");
+    container.scrollTo(0, 0);
 }
