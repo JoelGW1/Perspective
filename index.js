ff4246e60a08838c28b42814814a880a92c91dd5
JoelGW1 <155395766+JoelGW1@users.noreply.github.com>
2024-01-28 19:57:13 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,139 +1,114 @@
-let stars = document.getElementById("stars");
-let s1 = document.getElementsByClassName("s1");
-let s2 = document.getElementsByClassName("s2");
-let s3 = document.getElementsByClassName("s3");
-let s4 = document.getElementsByClassName("s4");
-let lifeSpan = 80;
-let age = document.getElementById("age").value = 0;
+const lifeExpectancy = 80;
 
-
-function drawSky(years) {
-    {
-
-        for (let i = 0; i < ((years*0.5)*52); i++) {
-            stars.insertAdjacentHTML("afterbegin","<div class='s1'> </div>");
-        }
-        
-        for (let i = 0; i < s1.length; i++) {
-            drawStar(s1, i, 0);
-        }
-        }
-        
-        {
-        for (let i = 0; i < ((years*0.4)*52); i++) {
-            stars.insertAdjacentHTML("afterbegin","<div class='s2'> </div>");
-        }
-        
-        for (let i = 0; i < s2.length; i++) {
-            drawStar(s2, i, 0);
-        }
-        }
-        
-        {
-        for (let i = 0; i < ((years*0.05)*52); i++) {
-            stars.insertAdjacentHTML("afterbegin","<div class='s3'> </div>");
-        }
-        
-        for (let i = 0; i < s3.length; i++) {
-            drawStar(s3, i, 0);
-        }
-        }
-        
-        {
-        for (let i = 0; i < ((years*0.05)*52); i++) {
-            stars.insertAdjacentHTML("afterbegin","<div class='s4'> </div>");
-        }
-        
-        for (let i = 0; i < s4.length; i++) {
-            drawStar(s4, i, 0);
-        }
-        }
-        setTimeout(start,1);
+function showCard(number) {
+    document.getElementById(number).style.opacity = "1";
 }
 
-function drawStar(type, i, opacityV) {
-    let thisStar = type[i];
-    randomTop = getRandomPosition(0, window.innerHeight -5);
-    randomLeft = getRandomPosition(0, window.innerWidth -5);
-    thisStar.style.top = randomTop+"px";
-    thisStar.style.left = randomLeft+"px";
-    thisStar.style.opacity = opacityV;
+function hideCard(number) {
+    document.getElementById(number).style.display = "none";
 }
+
+for (let i = 0; i < lifeExpectancy * 52; i++){
+    document.getElementById("sky").insertAdjacentHTML("afterbegin",`<div class="star"></div>`);
+}
+
+let star = document.getElementsByClassName("star");
 
 function getRandomPosition(min,max) {
     return Math.random() * (max-min) + min;
 }
-function play() {
-    document.getElementById("two").scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"});
+
+for (let i=0;i< star.length; i++) {
+    star[i].style.top = "49.5%";
+    star[i].style.left = "49.5%"; 
+    if (i < 0.5*star.length) {
+        star[i].style.width = "1px";
+        star[i].style.height = "1px";
+        star[i].style.opacity = "0.5";
+        star[i].style.transition = "transform 0.6s ease-in-out";
+    } else if (i < 0.9*star.length){
+        star[i].style.width = "2px";
+        star[i].style.height = "2px";
+        star[i].style.opacity = "0.6";
+        star[i].style.transition = "transform 1.0s ease-in-out";
+    } else if (i < 0.97*star.length){
+        star[i].style.width = "3px";
+        star[i].style.height = "3px";
+        star[i].style.opacity = "0.7";
+        star[i].style.transition = "transform 1.3s ease-in-out";
+    } else {
+        star[i].style.width = "4px";
+        star[i].style.height = "4px";
+        star[i].style.opacity = "1";
+        star[i].style.transition = "transform 1.7s ease-in-out";
+    }
+    setTimeout(function() {star[i].style.transform = `translate(${getRandomPosition(-window.innerWidth/2,window.innerWidth/2)}px,${getRandomPosition(-window.innerHeight/2,window.innerHeight/2)}px)`},1);
 }
-function start() {
-    let age = document.getElementById("age").value;
-    age = Number(age);
-    if (age <= 80 && age >= 0){
-        for (let i = 0; i < lifeSpan*0.5*52; i++) {
-            s1[i].style.opacity = '0.6';
-        }
-        for (let i = 0; i < lifeSpan*0.4*52; i++) {
-            s2[i].style.opacity = '0.7';
-        }
-        for (let i = 0; i < lifeSpan*0.05*52; i++) {
-            s3[i].style.opacity = '0.8';
-        }
-        for (let i = 0; i < lifeSpan*0.05*52; i++) {
-            s4[i].style.opacity = '1';
-        }
-        
-        for (let i = 0; i < age*0.5*52; i++) {
-            s1[i].style.opacity = '0';
-        }
-        for (let i = 0; i < age*0.4*52; i++) {
-            s2[i].style.opacity = '0';
-        }
-        for (let i = 0; i < age*0.05*52; i++) {
-            s3[i].style.opacity = '0';
-        }
-        for (let i = 0; i < age*0.05*52; i++) {
-            s4[i].style.opacity = '0';
-        }
+
+function drawCircle() {
+    let weeks = Number(document.getElementById("input").value) * 52;
+    for (let i = 0;i < star.length; i++) {
+        if (i < weeks) {
+            star[i].style.width = "3px";
+            star[i].style.height = "3px";
+            star[i].style.transition = "opacity 0.6s ease-in";
+            star[i].style.opacity = "0";
         } else {
-            alert("age >= 0 or age <= 80");
+            let n = i - weeks + 1;
+            star[i].style.width = "5px";
+            star[i].style.height = "5px";
+            star[i].style.opacity = "0.4";
+            star[i].style.transition = "transform 1s ease-in-out";
+            star[i].style.transform = `translate(${5*Math.sqrt(n)*Math.cos(n/2)}px,${5*Math.sqrt(n)*Math.sin(n/2)}px)`;
+        }
     }
 }
 
-function animate2() {
-    if (age >=0 && age<=80){
-    document.getElementById("one").innerHTML = ``;
-    age = Number(document.getElementById("age").value);
-    start();
-    document.getElementById("two").innerHTML = `<h1>These are your remaining weeks</h1>`;
-    setTimeout(function() {
-    for (let i =0; i <= (lifeSpan-age);i++){
-        setTimeout(function() {
-        document.getElementById("two").innerHTML = `<h1 id="counter">${age} years</h1>`;
-        console.log(age);
-        for (let i = 0; i < age*0.5*52; i++) {
-            s1[i].style.transition = "opacity 0.1s ease-in-out";
-            s1[i].style.opacity = '0';
+function drawLive() {
+    let weeks = Number(document.getElementById("input").value) * 52;
+    for (let i=weeks;i< star.length; i++) {
+        star[i].style.background = "white";
+        if (i < 0.5*star.length) {
+            star[i].style.width = "1px";
+            star[i].style.height = "1px";
+            star[i].style.opacity = "0.5";
+            star[i].style.transition = "transform 0.6s ease-in-out";
+        } else if (i < 0.9*star.length){
+            star[i].style.width = "2px";
+            star[i].style.height = "2px";
+            star[i].style.opacity = "0.6";
+            star[i].style.transition = "transform 1.0s ease-in-out";
+        } else if (i < 0.97*star.length){
+            star[i].style.width = "3px";
+            star[i].style.height = "3px";
+            star[i].style.opacity = "0.7";
+            star[i].style.transition = "transform 1.3s ease-in-out";
+        } else {
+            star[i].style.width = "4px";
+            star[i].style.height = "4px";
+            star[i].style.opacity = "1";
+            star[i].style.transition = "transform 1.7s ease-in-out";
         }
-        for (let i = 0; i < age*0.4*52; i++) {
-            s2[i].style.transition = "opacity 0.1s ease-in-out";
-            s2[i].style.opacity = '0';
-        }
-        for (let i = 0; i < age*0.05*52; i++) {
-            s3[i].style.transition = "opacity 0.1s ease-in-out";
-            s3[i].style.opacity = '0';
-        }
-        for (let i = 0; i < age*0.05*52; i++) {
-            s4[i].style.transition = "opacity 0.1s ease-in-out";
-            s4[i].style.opacity = '0';
-        }
-        if (age == 80) {setTimeout(function() {location.reload()},1500)}
-        age++;
-        }, i*200)};}, 2500);
+        setTimeout(function() {star[i].style.transform = `translate(${getRandomPosition(-window.innerWidth/2,window.innerWidth/2)}px,${getRandomPosition(-window.innerHeight/2,window.innerHeight/2)}px)`},1);
     }
 }
 
-function toTop() {
-    let container = document.getElementById("container");
-    container.scrollTo(0, 0);
+function color() {
+    let weeks = Number(document.getElementById("input").value) * 52;
+    for (let i=weeks;i< star.length; i++) {
+        star[i].style.transition = "background 1s ease-in-out";
+        star[i].style.opacity = "0.6";
+        if (i < weeks+(0.3*(star.length-weeks))) {
+            star[i].style.background = "mediumseagreen";
+        }
+        else if (i < weeks+(0.6*(star.length-weeks))) {
+            star[i].style.background = "orange";
+        }
+        else if (i < weeks+(0.8*(star.length-weeks))) {
+            star[i].style.background = "slateblue";
+        }
+        else if (i < weeks+(1*(star.length-weeks))) {
+            star[i].style.background = "tomato";
+        }
+    }
 }
